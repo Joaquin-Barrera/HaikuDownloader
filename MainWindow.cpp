ed9def8246c91eac2a2a0b806e369d54affa5a74
Joaquin-Barrera <franplan26@gmail.com>
2026-09-05 10:55:53 +0000

    Bypass YouTube bot detection and improve cancellation feedback

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -12,26 +12,33 @@
 #include <FindDirectory.h>
 #include <Alert.h>
 #include <StringView.h>
-#include <Notification.h> 
+#include <Notification.h>
+#include <LocaleRoster.h>
 #include <cstdio>
 #include <unistd.h>
 #include <stdlib.h>
+
+bool gIsSpanish = false;
 
 // --- Folder Dialog ---
 class FolderDialog : public BWindow {
 public:
     FolderDialog(bool* create, bool* neverAsk, sem_id sem) 
-        : BWindow(BRect(0,0,1,1), "Downloads Folder", B_MODAL_WINDOW, 
+        : BWindow(BRect(0,0,1,1), (gIsSpanish ? "Carpeta de Descargas" : "Downloads Folder"), B_MODAL_WINDOW, 
                   B_NOT_RESIZABLE | B_AUTO_UPDATE_SIZE_LIMITS | B_CLOSE_ON_ESCAPE),
           fCreate(create), fNeverAsk(neverAsk), fSem(sem) {
+        
+        BStringView* msg = new BStringView("m", gIsSpanish ? "La carpeta Downloads no existe. ¿Desea crearla?" 
+                                                          : "The Downloads folder does not exist. Create it?");
+        fCheck = new BCheckBox("n", gIsSpanish ? "No volver a preguntar" : "Don't ask again", nullptr);
+        BButton* btnCancel = new BButton("c", gIsSpanish ? "Cancelar" : "Cancel", new BMessage('no'));
+        BButton* btnCreate = new BButton("ok", gIsSpanish ? "Crear" : "Create", new BMessage('yes'));
+        btnCreate->MakeDefault(true);
+
         BLayoutBuilder::Group<>(this, B_VERTICAL, B_USE_DEFAULT_SPACING)
             .SetInsets(B_USE_WINDOW_INSETS)
-            .Add(new BStringView("m", "The Downloads folder does not exist. Create it?"))
-            .Add(fCheck = new BCheckBox("n", "Don't ask again", nullptr))
-            .AddGroup(B_HORIZONTAL).AddGlue()
-                .Add(new BButton("c", "Cancel", new BMessage('no')))
-                .Add(new BButton("ok", "Create", new BMessage('yes')))
-            .End();
+            .Add(msg).Add(fCheck).AddStrut(5)
+            .AddGroup(B_HORIZONTAL).AddGlue().Add(btnCancel).Add(btnCreate).End();
         CenterOnScreen();
     }
     void MessageReceived(BMessage* m) {
@@ -51,22 +58,25 @@ MainWindow::MainWindow()
               B_ASYNCHRONOUS_CONTROLS | B_AUTO_UPDATE_SIZE_LIMITS),
       fDownloadThread(-1), fIsDownloading(false) {
 
+    BMessage languages;
+    BLocaleRoster::Default()->GetPreferredLanguages(&languages);
+    BString lang;
+    if (languages.FindString("language", &lang) == B_OK) {
+        if (lang.StartsWith("es")) gIsSpanish = true;
+    }
+
     _LoadSettings();
 
-    // 1. Top Controls
     fUrlInput = new BTextControl("url", "URL:", "", nullptr);
-    // Note: SetCueText was removed for compatibility as discussed
+    fPasteBtn = new BButton("paste", gIsSpanish ? "Pegar" : "Paste", new BMessage(MSG_PASTE));
     
-    fPasteBtn = new BButton("paste", "Paste", new BMessage(MSG_PASTE));
-    
-    // 2. Format and Quality Selectors
     BPopUpMenu* fmtMenu = new BPopUpMenu("f");
     BMessage* mVid = new BMessage(MSG_MODE_CHANGED); mVid->AddInt32("i", 0);
     BMessage* mAud = new BMessage(MSG_MODE_CHANGED); mAud->AddInt32("i", 1);
-    fmtMenu->AddItem(new BMenuItem("Video (MP4/MKV)", mVid));
-    fmtMenu->AddItem(new BMenuItem("Audio Only", mAud));
+    fmtMenu->AddItem(new BMenuItem(gIsSpanish ? "Video (MP4/MKV)" : "Video (MP4/MKV)", mVid));
+    fmtMenu->AddItem(new BMenuItem(gIsSpanish ? "Solo Audio" : "Audio Only", mAud));
     fmtMenu->ItemAt(0)->SetMarked(true);
-    fFormatMenu = new BMenuField("Mode:", fmtMenu);
+    fFormatMenu = new BMenuField(gIsSpanish ? "Modo:" : "Mode:", fmtMenu);
 
     BPopUpMenu* qMenu = new BPopUpMenu("q");
     qMenu->AddItem(new BMenuItem("1080p", nullptr));
@@ -74,47 +84,34 @@ MainWindow::MainWindow()
     qMenu->AddItem(new BMenuItem("480p", nullptr));
     qMenu->AddItem(new BMenuItem("360p", nullptr));
     qMenu->ItemAt(1)->SetMarked(true);
-    fQualityMenu = new BMenuField("Quality:", qMenu);
+    fQualityMenu = new BMenuField(gIsSpanish ? "Calidad:" : "Quality:", qMenu);
 
     BPopUpMenu* afMenu = new BPopUpMenu("af");
     afMenu->AddItem(new BMenuItem("MP3", nullptr));
-    afMenu->AddItem(new BMenuItem("FLAC (Lossless)", nullptr));
-    afMenu->AddItem(new BMenuItem("M4A (Native)", nullptr));
+    afMenu->AddItem(new BMenuItem("FLAC", nullptr));
+    afMenu->AddItem(new BMenuItem("M4A", nullptr));
     afMenu->ItemAt(0)->SetMarked(true);
-    fAudioFormatMenu = new BMenuField("Audio Format:", afMenu);
+    fAudioFormatMenu = new BMenuField(gIsSpanish ? "Formato Audio:" : "Audio Format:", afMenu);
     fAudioFormatMenu->Hide(); 
 
-    // 3. Progress Bar
-    fStatusBar = new BStatusBar("progress", "Waiting...");
+    fStatusBar = new BStatusBar("progress", gIsSpanish ? "Esperando..." : "Waiting...");
     fStatusBar->SetMaxValue(100.0);
-
     fLogView = new BTextView("log");
     fLogView->MakeEditable(false);
     fScrollView = new BScrollView("s", fLogView, 0, false, true);
 
-    fStopBtn = new BButton("stop", "Stop", new BMessage(MSG_STOP));
+    fStopBtn = new BButton("stop", gIsSpanish ? "Detener" : "Stop", new BMessage(MSG_STOP));
     fStopBtn->SetEnabled(false);
-    fDownloadBtn = new BButton("down", "Download", new BMessage(MSG_DOWNLOAD));
+    fDownloadBtn = new BButton("down", gIsSpanish ? "Descargar" : "Download", new BMessage(MSG_DOWNLOAD));
     fDownloadBtn->MakeDefault(true);
 
-    // Layout
     BLayoutBuilder::Group<>(this, B_VERTICAL, B_USE_SMALL_SPACING)
         .SetInsets(B_USE_WINDOW_INSETS)
-        .AddGroup(B_HORIZONTAL, B_USE_DEFAULT_SPACING)
-            .Add(fUrlInput, 10).Add(fPasteBtn, 0)
-        .End()
-        .AddGroup(B_HORIZONTAL, B_USE_DEFAULT_SPACING)
-            .Add(fFormatMenu)
-            .Add(fQualityMenu)
-            .Add(fAudioFormatMenu)
-            .AddGlue()
-        .End()
+        .AddGroup(B_HORIZONTAL, B_USE_DEFAULT_SPACING).Add(fUrlInput, 10).Add(fPasteBtn, 0).End()
+        .AddGroup(B_HORIZONTAL, B_USE_DEFAULT_SPACING).Add(fFormatMenu).Add(fQualityMenu).Add(fAudioFormatMenu).AddGlue().End()
         .AddStrut(5).Add(new BSeparatorView(B_HORIZONTAL)).AddStrut(5)
-        .Add(fStatusBar)
-        .Add(fScrollView, 100)
-        .AddGroup(B_HORIZONTAL, B_USE_DEFAULT_SPACING)
-            .Add(fStopBtn).AddGlue().Add(fDownloadBtn)
-        .End();
+        .Add(fStatusBar).Add(fScrollView, 100)
+        .AddGroup(B_HORIZONTAL, B_USE_DEFAULT_SPACING).Add(fStopBtn).AddGlue().Add(fDownloadBtn).End();
 
     CenterOnScreen();
 }
@@ -125,11 +122,9 @@ void MainWindow::MessageReceived(BMessage* m) {
     if (m->WasDropped()) {
         const char* url;
         if (m->FindString("_data", &url) == B_OK || m->FindString("text/plain", &url) == B_OK) {
-            fUrlInput->SetText(url);
-            return;
+            fUrlInput->SetText(url); return;
         }
     }
-
     switch (m->what) {
         case MSG_MODE_CHANGED: {
             int32 i; m->FindInt32("i", &i);
@@ -138,60 +133,48 @@ void MainWindow::MessageReceived(BMessage* m) {
             break;
         }
         case MSG_PROGRESS_UPDATE: {
-            float percent; m->FindFloat("p", &percent);
-            const char* text; m->FindString("t", &text);
-            fStatusBar->Update(percent - fStatusBar->CurrentValue(), text);
+            float p; m->FindFloat("p", &p); 
+            const char* t; 
+            if (m->FindString("t", &t) == B_OK) {
+                fStatusBar->Update(p - fStatusBar->CurrentValue(), t); 
+            }
             break;
         }
         case MSG_PASTE: _PasteFromClipboard(); break;
         case MSG_DOWNLOAD: _CheckUrlAndStart(); break;
         case MSG_STOP: _StopDownload(); break;
         case MSG_OUTPUT_RECEIVED: {
-            const char* t; if (m->FindString("text", &t) == B_OK) _UpdateLog(t);
-            break;
+            const char* t; if (m->FindString("text", &t) == B_OK) _UpdateLog(t); break;
         }
-        case MSG_DOWNLOAD_FINISHED: {
-            fIsDownloading = false;
-            fStopBtn->SetEnabled(false);
-            fDownloadBtn->SetEnabled(true);
-            fUrlInput->SetEnabled(true);
-            
+        case MSG_DOWNLOAD_FINISHED:
+            fIsDownloading = false; fStopBtn->SetEnabled(false);
+            fDownloadBtn->SetEnabled(true); fUrlInput->SetEnabled(true);
             if (fDownloadThread != -1) {
-                fStatusBar->SetTo(100.0, "Finished!");
-                _SendNotification("HaikuDownloader", "Download completed successfully.");
+                fStatusBar->SetTo(100.0, gIsSpanish ? "¡Finalizado!" : "Finished!");
+                _SendNotification("HaikuDownloader", gIsSpanish ? "Descarga completada." : "Download completed.");
                 fDownloadThread = -1;
             }
             break;
-        }
         default: BWindow::MessageReceived(m); break;
     }
 }
 
 void MainWindow::_UpdateLog(const char* text) {
-    fLogView->Insert(text);
-    fLogView->ScrollToOffset(fLogView->TextLength());
-
-    BString s(text);
-    int32 pos = s.FindFirst("%");
+    fLogView->Insert(text); fLogView->ScrollToOffset(fLogView->TextLength());
+    BString s(text); int32 pos = s.FindFirst("%");
     if (pos != B_ERROR && pos > 4) {
-        BString pStr;
-        s.CopyInto(pStr, pos-4, 4);
-        float p = atof(pStr.String());
+        BString pStr; s.CopyInto(pStr, pos-4, 4); float p = atof(pStr.String());
         if (p > 0) {
-            BMessage msg(MSG_PROGRESS_UPDATE);
-            msg.AddFloat("p", p);
-            msg.AddString("t", "Downloading...");
+            BMessage msg(MSG_PROGRESS_UPDATE); msg.AddFloat("p", p);
+            msg.AddString("t", gIsSpanish ? "Descargando..." : "Downloading...");
             PostMessage(&msg);
         }
     }
 }
 
 void MainWindow::_SendNotification(const char* title, const char* content) {
-    BNotification notification(B_INFORMATION_NOTIFICATION);
-    notification.SetGroup("HaikuDownloader");
-    notification.SetTitle(title);
-    notification.SetContent(content);
-    notification.Send();
+    BNotification n(B_INFORMATION_NOTIFICATION);
+    n.SetGroup("HaikuDownloader"); n.SetTitle(title); n.SetContent(content); n.Send();
 }
 
 void MainWindow::_CheckUrlAndStart() {
@@ -200,40 +183,42 @@ void MainWindow::_CheckUrlAndStart() {
     if (!_EnsureDownloadPath()) return;
 
     fLogView->SetText(""); 
-    fStatusBar->SetTo(0, "Starting...");
+    fStatusBar->Reset(gIsSpanish ? "Iniciando..." : "Starting...");
 
     if (url.FindFirst("list=") != B_ERROR) {
-        BString check = "yt-dlp --ignore-config --flat-playlist --playlist-items 1..2 --get-id \"";
+        // En la verificación también quitamos webpositive y usamos extractor-args
+        BString check = "yt-dlp --ignore-config --extractor-args \"youtube:player_client=ios,android,web\" --flat-playlist --playlist-items 1..2 --get-id \"";
         check << url << "\"";
         FILE* p = popen(check.String(), "r"); int count = 0; char b[128];
         if (p) { while (fgets(b, sizeof(b), p)) count++; pclose(p); }
         if (count > 1) {
-            (new BAlert("!", "Playlists are not allowed.", "OK"))->Go();
-            fDownloadBtn->SetEnabled(true); fUrlInput->SetEnabled(true);
-            return;
+            (new BAlert("!", gIsSpanish ? "No se permiten listas." : "Playlists not allowed.", "OK"))->Go();
+            fDownloadBtn->SetEnabled(true); fUrlInput->SetEnabled(true); return;
         }
     }
     _StartDownload();
 }
 
 void MainWindow::_StartDownload() {
-    fIsDownloading = true; 
-    fStopBtn->SetEnabled(true);
-    fDownloadBtn->SetEnabled(false); 
-    fUrlInput->SetEnabled(false);
+    fIsDownloading = true; fStopBtn->SetEnabled(true);
+    fDownloadBtn->SetEnabled(false); fUrlInput->SetEnabled(false);
     fDownloadThread = spawn_thread(_DownloadRunner, "dl", B_NORMAL_PRIORITY, this);
-    if (fDownloadThread >= B_OK) resume_thread(fDownloadThread);
+    resume_thread(fDownloadThread);
 }
 
 void MainWindow::_StopDownload() {
-    if ((new BAlert("?", "Cancel download?", "No", "Yes"))->Go() == 1) {
+    BAlert* a = new BAlert("?", gIsSpanish ? "¿Cancelar descarga?" : "Cancel download?", 
+                           gIsSpanish ? "No" : "No", gIsSpanish ? "Sí" : "Yes");
+    if (a->Go() == 1) {
         system("pkill -9 yt-dlp"); 
         fDownloadThread = -1;
         snooze(200000); 
         _CleanupCorruptFiles();
+        _UpdateLog(gIsSpanish ? "\n🛑 Descarga cancelada por el usuario.\n" 
+                              : "\n🛑 Download cancelled by the user.\n");
         fIsDownloading = false; fStopBtn->SetEnabled(false);
         fDownloadBtn->SetEnabled(true); fUrlInput->SetEnabled(true);
-        fStatusBar->SetTo(0, "Canceled.");
+        fStatusBar->Reset(gIsSpanish ? "Esperando..." : "Waiting...");
     }
 }
 
@@ -245,48 +230,32 @@ void MainWindow::_CleanupCorruptFiles() {
     }
 }
 
-int32 MainWindow::_DownloadRunner(void* data) {
-    MainWindow* win = (MainWindow*)data;
-    win->Lock();
-    BString url = win->fUrlInput->Text();
-    BString path = win->fFinalDownloadPath;
-    int32 mode = win->fFormatMenu->Menu()->IndexOf(win->fFormatMenu->Menu()->FindMarked());
-    int32 qIdx = win->fQualityMenu->Menu()->IndexOf(win->fQualityMenu->Menu()->FindMarked());
-    int32 aFmt = win->fAudioFormatMenu->Menu()->IndexOf(win->fAudioFormatMenu->Menu()->FindMarked());
-    win->Unlock();
-
-    BString cmd = "yt-dlp --newline --progress --no-playlist -P \"";
-    cmd << path << "\" ";
-    
-    if (mode == 1) {
-        const char* formats[] = {"mp3", "flac", "m4a"};
-        cmd << "-x --audio-format " << formats[aFmt] << " ";
-    } else {
-        const char* res[] = {"1080", "720", "480", "360"};
-        cmd << "-f \"bestvideo[height<=" << res[qIdx] << "]+bestaudio/best\" --merge-output-format mp4 ";
+bool MainWindow::_EnsureDownloadPath() {
+    BPath p; find_directory(B_USER_DIRECTORY, &p); BPath dl = p; dl.Append("Downloads");
+    BEntry e(dl.Path());
+    if (e.Exists() && e.IsDirectory()) { fFinalDownloadPath = dl.Path(); return true; }
+    if (fNeverAskAgain) {
+        fFinalDownloadPath = fAlwaysCreateFolder ? dl.Path() : p.Path();
+        if (fAlwaysCreateFolder) create_directory(dl.Path(), 0777);
+        return true;
     }
-    
-    cmd << "\"" << url << "\" 2>&1";
-
-    FILE* pipe = popen(cmd.String(), "r");
-    if (pipe) {
-        char buffer[512];
-        while (fgets(buffer, sizeof(buffer), pipe)) {
-            if (win->fDownloadThread == -1) break;
-            BMessage* msg = new BMessage(MSG_OUTPUT_RECEIVED);
-            msg->AddString("text", buffer);
-            win->PostMessage(msg);
-        }
-        pclose(pipe);
-    }
-    win->PostMessage(MSG_DOWNLOAD_FINISHED);
-    return 0;
+    bool create = false;
+    sem_id s = create_sem(0, "w");
+    (new FolderDialog(&create, &fNeverAskAgain, s))->Show();
+    acquire_sem(s);
+    if (fNeverAskAgain) _SaveSettings(); 
+    fFinalDownloadPath = create ? dl.Path() : p.Path();
+    if (create) create_directory(dl.Path(), 0777);
+    return true;
 }
 
 void MainWindow::_LoadSettings() {
     BPath p; if (find_directory(B_USER_SETTINGS_DIRECTORY, &p) == B_OK) {
         p.Append("HaikuDownloader_settings"); BFile f(p.Path(), B_READ_ONLY); BMessage s;
-        if (s.Unflatten(&f) == B_OK) { fNeverAskAgain = s.GetBool("n", false); fAlwaysCreateFolder = s.GetBool("a", false); }
+        if (s.Unflatten(&f) == B_OK) {
+            fNeverAskAgain = s.GetBool("n", false);
+            fAlwaysCreateFolder = s.GetBool("a", false);
+        }
     }
 }
 
@@ -297,17 +266,44 @@ void MainWindow::_SaveSettings() {
     }
 }
 
-bool MainWindow::_EnsureDownloadPath() {
-    BPath p; find_directory(B_USER_DIRECTORY, &p); BPath dl = p; dl.Append("Downloads");
-    BEntry e(dl.Path()); if (e.Exists()) { fFinalDownloadPath = dl.Path(); return true; }
-    bool create = fAlwaysCreateFolder;
-    if (!fNeverAskAgain) {
-        sem_id s = create_sem(0, "w"); (new FolderDialog(&create, &fNeverAskAgain, s))->Show();
-        acquire_sem(s); if (fNeverAskAgain) { fAlwaysCreateFolder = create; _SaveSettings(); }
+int32 MainWindow::_DownloadRunner(void* data) {
+    MainWindow* win = (MainWindow*)data; win->Lock();
+    BString url = win->fUrlInput->Text(); BString path = win->fFinalDownloadPath;
+    int32 mode = win->fFormatMenu->Menu()->IndexOf(win->fFormatMenu->Menu()->FindMarked());
+    int32 qIdx = win->fQualityMenu->Menu()->IndexOf(win->fQualityMenu->Menu()->FindMarked());
+    int32 aFmt = win->fAudioFormatMenu->Menu()->IndexOf(win->fAudioFormatMenu->Menu()->FindMarked());
+    win->Unlock();
+
+    // --- NUEVA ESTRATEGIA DE BYPASS ---
+    BString cmd = "yt-dlp --newline --progress --no-playlist ";
+    // Usamos extractor-args para engañar a YT diciendo que somos un dispositivo móvil
+    cmd << "--extractor-args \"youtube:player_client=ios,android,web\" ";
+    cmd << "--user-agent \"Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1\" ";
+    cmd << "-P \"" << path << "\" ";
+
+    if (mode == 1) { 
+        const char* f[] = {"mp3", "flac", "m4a"}; 
+        cmd << "-x --audio-format " << f[aFmt] << " "; 
+    } else { 
+        const char* r[] = {"1080", "720", "480", "360"}; 
+        cmd << "-f \"bestvideo[height<=" << r[qIdx] << "]+bestaudio/best\" --merge-output-format mp4 "; 
     }
-    fFinalDownloadPath = create ? dl.Path() : p.Path();
-    if (create) create_directory(dl.Path(), 0777);
-    return true;
+
+    cmd << "\"" << url << "\" 2>&1";
+
+    FILE* pipe = popen(cmd.String(), "r");
+    if (pipe) { 
+        char b[512]; 
+        while (fgets(b, sizeof(b), pipe)) { 
+            if (win->fDownloadThread == -1) break; 
+            BMessage msg(MSG_OUTPUT_RECEIVED); 
+            msg.AddString("text", b); 
+            win->PostMessage(&msg); 
+        } 
+        pclose(pipe); 
+    }
+    win->PostMessage(MSG_DOWNLOAD_FINISHED); 
+    return 0;
 }
 
 void MainWindow::_PasteFromClipboard() {
